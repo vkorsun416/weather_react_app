cc78b259d088a296559d6e60b57db895f8121d24
vkorsun416 <vkorsun416@gmail.com>
2022-04-30 17:58:48 +0300

    fix error with slow fetch data

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -10,22 +10,26 @@ function ApiContext(props) {
     const [weatherData, setWeatherData] = useState([]);
 
     function getUserLocation() {
-
         navigator.geolocation.getCurrentPosition(position => {
             setLat(position.coords.latitude);
             setLong(position.coords.longitude);
         }, () => {
-            // Riga, Latvia
+            // Coordinates of Riga, Latvia
             setLat(57);
             setLong(25);
         });
     }
 
     async function getWeatherData() {
-        await fetch(`${process.env.REACT_APP_WEATHER_API_URL}/forecast/?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+        return await fetch(`${process.env.REACT_APP_WEATHER_API_URL}/forecast/?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
             .then(res => res.json())
             .then(result => {
-                setWeatherData(result)
+                if (result.cod === '200') {
+                    setWeatherData(result);
+                }
+            })
+            .catch(err => {
+                console.log(err)
             });
     }
 
